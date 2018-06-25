a1d7aaa2ee895a0996f8ac76445387a3215273b7
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-25 15:25:58 -0600

    Created class for tomagotchi.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 console.log("JavaScript is running...");
 
+class Pet {
+	constuctor(name, gender, hunger, fatigue, bordedom) {
+		this.name = name;
+		this.gender = gender;
+		this.hunger = hunger;
+		this.fatigue = fatigue;
+		this.bordedom = bordedom;
+	}
+};
+
 
 
 
