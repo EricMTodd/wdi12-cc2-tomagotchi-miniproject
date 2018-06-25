288a85ad55d74d89f16c9a2ff5cb5fa676ea3449
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-25 15:38:59 -0600

    Instantiated new pet.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 console.log("JavaScript is running...");
 
 class Pet {
-	constuctor(name, gender, hunger, fatigue, bordedom) {
+	constructor(name, gender, age, hunger, fatigue, bordedom) {
 		this.name = name;
 		this.gender = gender;
+		this.age = age;
 		this.hunger = hunger;
 		this.fatigue = fatigue;
 		this.bordedom = bordedom;
 	}
 };
+
+const tomagotchi = new Pet("leeroy", "male", 0, 0, 0, 0);
+console.log(tomagotchi);
 
 
 
