b3c5b0cd29d5b4de73e9fe354d7732f5966631bb
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-26 16:41:53 -0600

    Pushing to desktop for further work.

--- js/app.js
+++ js/app.js
@@ -7,13 +7,12 @@ console.log("JavaScript is running...");
 // Most Functional
 
 // First, create everything that makes the game run.
-// Second, create a game object that will run the game.
+// Second, create a game object that will run the game, make the game more efficient.
 // Style the fuck out of it.
 
 // Developer note: Everything is always subject to change at any time for any reason.
 
 // Class to create new pet.
-
 class Pet {
 	constructor(name, gender, age, hunger, fatigue, bordedom) {
 		this.name = name;
@@ -34,7 +33,12 @@ let age = tomagotchi.age;
 let fatigue = tomagotchi.fatigue;
 let hunger = tomagotchi.hunger;
 let boredom = tomagotchi.boredom;
-let minute = 60000;
+let conscious = true;
+let seconds = 0;
+
+// Timeframe variables
+let secondCounter = 1;
+let minuteCounter = 1;
 
 // jQuery variables for HTML references
 $feed = $("#feed");
@@ -64,68 +68,96 @@ $("#hunger").text(`H: ${hunger}/10`);
 $("#fatigue").text(`F: ${fatigue}/10`);
 $("#boredom").text(`B: ${boredom}/10`);
 
-// Conditions that result in game objective failure.
+// Global timer to control status effects increase.
 
-// Timer that increases tomagotchi's age by 1 every hour. The tomagotchi will die at the start of the 5th hour.
-const ageAlgorithm = () => {
-	if (age < ageMax) {
-		age++;
-		console.log(`${petName} is ${age}.`);
-		$("#age").text(`A: ${age}`);
+const timeLapse = () => {
+		if (secondCounter < 60) {
+			secondCounter++;
+		console.log(`second ${secondCounter}`);
 	} else {
-		clearInterval(ageTimer);
-		console.log("ageTimer Cleared");
-		alert(`Game Over! ${petName} has died of old age.`);
+		minuteCounter++;
+		secondCounter = 1;
+		console.log(`Minute ${minuteCounter}`);
 	}
 };
 
-console.log("ageAlgorithm is running...");
-const ageTimer = setInterval(ageAlgorithm, minute * 60);
+const timer = setInterval(timeLapse, 1000);
 
-// Timer that simulates how hungry tomagotchi is. If the gauge get's maxed out, tomagotchi will die. If you try to overfeed tomagotchi, an alert will display.
-const hungerAlgorithm = () => {
-	if (hunger < hungerMax) {
-		hunger++;
-		$("#hunger").text(`H: ${hunger}/10`);
-	} else {
-		clearInterval(hungerTimer);
-		console.log("hungerTimer Cleared");
-		alert(`Game Over! ${petName} has died of starvation.`)
-	}
-};
+// // Timer that increases tomagotchi's age by 1 every hour. The tomagotchi will die at the start of the 5th hour.
+// const ageAlgorithm = () => {
+// 	if (age < ageMax) {
+// 		age++;
+// 		console.log(`${petName} is ${age}.`);
+// 		$("#age").text(`A: ${age}`);
+// 	} else {
+// 		clearInterval(ageTimer);
+// 		console.log("ageTimer Cleared");
+// 		alert(`Game Over! ${petName} has died of old age.`);
+// 	}
+// };
 
-console.log("hungerAlgorithm is running...");
-const hungerTimer = setInterval(hungerAlgorithm, minute * 15);
+// console.log("ageAlgorithm is running...");
+// const ageTimer = setInterval(ageAlgorithm, minute * 60);
 
-// Timer that simulates fatigue over a set amount of time. If the fatigue guage is maxed out, tomagotchi will pass out. If you try to make him sleep while he is rested, an alert will display.
-const fatigueAlgorithm = () => {
-	if (fatigue < fatigueMax) {
-		fatigue++;
-		$("#fatigue").text(`F: ${fatigue}/10`);
-	} else {
-		clearInterval(fatigueTimer);
-		console.log("fatigueTimer Cleared");
-		alert(`${petName} has passed out from fatigue!`);
-	}
-};
+// // Timer that simulates how hungry tomagotchi is. If the gauge get's maxed out, tomagotchi will die. If you try to overfeed tomagotchi, an alert will display.
+// const hungerAlgorithm = () => {
+// 	if (hunger < hungerMax) {
+// 		hunger++;
+// 		$("#hunger").text(`H: ${hunger}/10`);
+// 	} else {
+// 		clearInterval(hungerTimer);
+// 		console.log("hungerTimer Cleared");
+// 		alert(`Game Over! ${petName} has died of starvation.`)
+// 	}
+// };
 
-console.log("fatigueAlgorithm is running...");
-const fatigueTimer = setInterval(fatigueAlgorithm, minute * 30);
+// console.log("hungerAlgorithm is running...");
+// const hungerTimer = setInterval(hungerAlgorithm, minute * 15);
 
-// Timer that simulates boredom. Exact same function as fatigue, with different time intervals.
-const boredomAlgorithm = () => {
-	if (boredom < boredomMax) {
-		boredom++;
-		$("#boredom").text(`B: ${boredom}/10`);
-	} else {
-		clearInterval(boredomTimer);
-		console.log("boredomTimer Cleared");
-		alert(`${petName} has passed out from sheer boredom!`);
-	}
-};
+// // Timer that simulates fatigue over a set amount of time. If the fatigue guage is maxed out, tomagotchi will pass out. If you try to make him sleep while he is rested, an alert will display.
+// const fatigueAlgorithm = () => {
+// 	if (fatigue < fatigueMax) {
+// 		fatigue++;
+// 		$("#fatigue").text(`F: ${fatigue}/10`);
+// 	} else {
+// 		// clearInterval(fatigueTimer);
+// 		// console.log("fatigueTimer Cleared");
+// 		alert(`${petName} has passed out from fatigue!`);
+// 		conscious = false;
+// 	}
+// };
 
-console.log("boredomAlgorithm is running...");
-const boredomTimer = setInterval(boredomAlgorithm, minute * 3);
+// console.log("fatigueAlgorithm is running...");
+// const fatigueTimer = setInterval(fatigueAlgorithm, 1000);
+
+// // Timer that allows the tomagotchi to rest when they become fatigued.
+// const restAlgorithm = () => {
+// 	if (conscious === false) {
+// 		clearInterval(fatigueTimer);
+// 		console.log("fatigueTimer Cleared");
+// 		fatigue--;
+// 		$("#fatigue").text(`F: ${fatigue}/10`);
+// 	}
+// 	if (fatigue === fatigueMax)
+// };
+
+// console.log("restAlgorithm is running...");
+// const restTimer = setInterval(restAlgorithm, 1000);
+
+// // Timer that simulates boredom. Exact same function as fatigue, with different time intervals.
+// const boredomAlgorithm = () => {
+// 	if (boredom < boredomMax) {
+// 		boredom++;
+// 		$("#boredom").text(`B: ${boredom}/10`);
+// 	} else {
+// 		clearInterval(boredomTimer);
+// 		console.log("boredomTimer Cleared");
+// 		alert(`${petName} has passed out from sheer boredom!`);
+// 	}
+// };
+
+// console.log("boredomAlgorithm is running...");
+// const boredomTimer = setInterval(boredomAlgorithm, minute * 3);
 
 // Buttons with which to interact with tomagotchi.
 
@@ -146,9 +178,6 @@ $lightSwitch.on("click", () => {
 		$("body").css("background-color", "black");
 		$("li").css("color", "white");
 		$("#petImg").attr("src", "");
-		$("#fatigue").text(`F: ${tomagotchi.fatigue}/10`);
-		clearInterval(fatigueTimer);
-		console.log("fatigueTimer Cleared");
 		lightToggle = true;
 	} else {
 		$("body").css("background-color", "orange");
